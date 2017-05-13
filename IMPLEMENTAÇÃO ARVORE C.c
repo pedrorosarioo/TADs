dabb1d74fec7402fcce8bc510525401ffaaf716f
pedrorosarioo <pedro.ufba2012@hotmail.com>
2017-05-13 14:22:53 -0300

    Update IMPLEMENTAÇÃO ARVORE C.c

--- IMPLEMENTAÇÃO ARVORE C.c
+++ IMPLEMENTAÇÃO ARVORE C.c
@@ -11,6 +11,11 @@ Arv arvVazia(){
     return NULL;
 }
 
+int maior(int x, int y){
+    if (x>=y) return x;
+    else return y;
+}
+
 Arv criaNo(char c, Arv a, Arv b){
     Arv aux= (Arv)malloc(sizeof(noArv));
     aux->chave=c;
@@ -29,7 +34,13 @@ void imprimeArv(Arv p){
     printf(">");
 }
 
-Arv liberaArv(Arv *p){
+void insereArvBB(Arv *p, Arv no){
+    if (!(*p)) *p=no;
+    else if((*p)->chave>no->chave) insereArvBB(&((*p)->esq), no);
+    else insereArvBB(&((*p)->dir), no);
+}
+
+Arv liberaArv(Arv *p){ //LIBERA UMA RAIZ E TODOS OS SEUS FILHOS
 Arv aux=*p;
     if(aux){
         liberaArv(&(aux->esq));
@@ -39,19 +50,48 @@ Arv aux=*p;
     return NULL;
 }
 
-int buscaArv(Arv p, char i){
+
+
+int buscaArv(Arv p, char i){ //VERIFICA SE O CARACTER i APARECE NA ARVORE
     if(p)
         return p->chave==i || buscaArv(p->esq, i) || buscaArv(p->dir, i);
     else
         return 0;
 }
 
+int buscanArv(Arv p, char i){ //PROCURA QUANTAS VEZES O CARACTER i SE REPETE NA ARVORE
+    if(p==NULL) return 0;
+    if (p->chave==i) return (1+buscanArv(p->esq, i)+buscanArv(p->dir, i));
+    else return (buscanArv(p->esq, i)+buscanArv(p->dir, i));
+}
+
+int igualArv(Arv p, Arv b){ //COMPARA SE O FORMATO DAS DUAS ARVORES SÃO IGUAIS
+    if ((!p)&&(!b)) return 1;
+    if ((!p)||(!b)) return 0;
+    else return (igualArv(p->esq, b->esq)*igualArv(p->dir, b->dir));
+}
+
+int simetricArv(Arv p, Arv b){ //VERIFICA SE DUAS ARVORES SAO SIMETRICAS
+    if ((!p)&&(!b)) return 1;
+    if ((!p)||(!b)) return 0;
+    else return (igualArv(p->esq, b->dir)*igualArv(p->dir, b->esq));
+}
+
+int alturaArv(Arv p){
+    if(p==NULL) return 0;
+    else return maior(1+alturaArv(p->esq), 1+alturaArv(p->dir));
+}
+
 int main(){
-Arv x;
-x=criaNo('a', criaNo('b', criaNo('c', arvVazia(), arvVazia()), arvVazia()), criaNo('d', arvVazia(), arvVazia()));
-imprimeArv(x);
+Arv x,y, k;
+
+x=criaNo('a', arvVazia(), criaNo('c', arvVazia(), arvVazia()));
+y=criaNo('b', arvVazia(), criaNo('d', arvVazia(), arvVazia()));
+insereArvBB(&x, y);
+//printf("%d", simetricArv(x, y));
 //x=liberaArv(&x);
-//imprimeArv(x);
-printf("\n%d", buscaArv(x, 'e'));
+imprimeArv(x);
+//printf("%d\n", alturaArv(x));
+//printf("\n%d", buscaArv(x, 'e'));
 return 0;
 }
